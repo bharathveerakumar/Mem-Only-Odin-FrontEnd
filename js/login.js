98789fbd1c8acdc69570477faf349910f038c681
bharathveerakumar <bharathveerakumars@gmail.com>
2022-01-27 19:34:11 +0530

    A

--- js/login.js
+++ js/login.js
@@ -23,7 +23,7 @@ btn.addEventListener('click', async (e)=>{
         })
         if(loginRes){
             let now=new Date(Date.now()+3000).toUTCString()
-            document.cookie="token="+loginRes.token+";"+"expires="+new Date(Date.now()+86400*24).toUTCString()
+            document.cookie="token="+loginRes.token+";"+"expires="+new Date(Date.now()+86400000).toUTCString()
             window.location.href='https://mem-only-odin-bvk.netlify.com/index.html'
         }
     }
